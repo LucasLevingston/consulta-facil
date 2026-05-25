14adee475e167f9c82d7dce7ef8156e8f575e842
Lucas Levingston <lucaolevingston@outlook.com>
2026-05-25 11:15:34 -0300

    fix:settings layout

--- src/app/(protected)/settings/layout.tsx
+++ src/app/(protected)/settings/layout.tsx
@@ -5,10 +5,7 @@ import { SettingsSidebar } from "./_components/settings-sidebar";
 export default function Layout({ children }: PropsWithChildren) {
 	return (
 		<div className="flex min-h-screen w-full flex-col">
-			<main className="bg-muted/40 flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
-				<div className="mx-auto grid w-full max-w-6xl gap-2">
-					<h1 className="text-3xl font-semibold">Configurações</h1>
-				</div>
+			<main>
 				<div className="mx-auto grid w-full  items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
 					<nav
 						className="text-muted-foreground grid gap-4 text-sm"
